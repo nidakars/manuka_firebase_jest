e8b6f7c3415e1f1ea05b06ad05cfc7c5d70b8fc5
nidakars <nidanurkars@gmail.com>
2021-01-23 04:17:08 +0300

    registerform-hatalı

---
+++ test/specs/RegisterForm.spec.js
@@ -0,0 +1,53 @@
+import { mount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import '../firebase'
+import RegisterForm from '../components/uye_kayit.vue'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+describe('RegisterForm', () => {
+    let wrapper;
+    const mockFunction = jest.fn();
+    beforeEach(() => {
+        wrapper = mount(RegisterForm, {
+            localVue,
+            data() {
+                return {
+
+                    email: 'redirectTest@test.com',
+                    password: '123456',
+                    ad: 'Test',
+                    soyad: 'Test',
+
+                };
+            },
+            store: new Vuex.Store({
+                actions: {
+                    register: mockFunction,
+                },
+            }),
+            mocks: {
+                $router: {
+                    push: jest.fn()
+                }
+            }
+        });
+    });
+
+    describe('methods', () => {
+        describe('#Kayit işlemi', () => {
+            it('register fonksiyonun çağrılması', async() => {
+                await wrapper.vm.register();
+                expect(mockFunction).toHaveBeenCalled();
+            });
+            /* 
+                  it("Successfull Register", () => {
+                    let registerButton = wrapper.find(".Kbtn");
+                    console.log = jest.fn();
+                    registerButton.trigger("click").then(() => {
+                      expect(console.log).toHaveBeenCalledWith('Kayıt Başarılı');
+                    });
+                  }); */
+        });
+    });
+});
